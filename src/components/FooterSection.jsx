6e229b147ecd247d45964c6b4c8aa16e9103e3c8
Santosh kumar gupta <sainkee1997@gmail.com>
2024-04-28 20:27:22 +0530

    footlink size changed

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -6,10 +6,10 @@ export default function FooterSection() {
       <div className="bg-neutral-800 py-10 mt-20  text-center">
         <p className="text-gray-500">{footer.desc}</p>
 
-        <ul className="flex flex-row justify-center gap-4 lg:py-10 mt-5 lg:mt-0">
+        <ul className="flex flex-row justify-center gap-2 lg:py-10 mt-5 lg:mt-0">
           {footer.footLinks.map((links, index) => (
             <li key={index} className="hover:text-blue-500">
-              <span className="hover:text-blue-500 text-lg">{links.label}</span>
+              <span className="hover:text-blue-500 text-sm">{links.label}</span>
             </li>
           ))}
         </ul>
@@ -19,7 +19,7 @@ export default function FooterSection() {
               key={index}
               className="hover:text-blue-500 border-2 p-2 hover:-translate-y-2 transition-all  border-orange-500 rounded-full"
             >
-              <span className="  cursor-pointer    text-sm text-orange-500">
+              <span className="  cursor-pointer    text-lg text-orange-500">
                 {icon.icon}
               </span>
             </li>
